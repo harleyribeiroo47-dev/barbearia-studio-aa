95cd72f5c4a81d6de9acc7adf0efa9468ef00783
harleyribeiroo47-dev <harleyribeiroo47@gmail.com>
2026-09-09 02:22:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='studio-aa-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='studio-aa-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
